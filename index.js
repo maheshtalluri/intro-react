2ded8981c14693b3afbe139edf422c5b00750317
Mahesh Kumar Talluri <tallurimahesh169@gmail.com>
2022-07-29 18:21:56 +0000

    REACT JS

---
+++ index.js
@@ -0,0 +1,8 @@
+const element = (
+  <div>
+    <h1 className="greeting">Hello Mahesh</h1>
+    <p>Good to see you here{10 - 3}</p>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
